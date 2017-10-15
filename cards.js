23bed67f819d797dd73a5b81898710a95eb1f8ef
Alex <alexanderhapp@me.com>
2017-10-15 11:36:32 -0700

    Add full responses to cards

--- cards.js
+++ cards.js
@@ -1,65 +1,70 @@
 module.exports = {
   ace: {
     numCards: 4,
-    response: "Waterfall",
+    response:
+      "Waterfall. Keep drinking until the person to right of you stops drinking.",
     audio:
       "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaTheBadTouchTechno.mp3"
   },
   two: {
     numCards: 4,
-    response: "You",
+    response: "You. Choose a person to take a drink!",
     audio:
       "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaSadTrombone.mp3"
   },
   three: {
     numCards: 4,
-    response: "Me",
+    response: "Me. Bummer you drew the card. You have to take a drink!",
     audio:
       "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaExtendedAirhorn.mp3"
   },
   four: {
     numCards: 4,
-    response: "Floor",
+    response: "Floor. QUICK GET ON THE FLOOR! LAST ONE ON THE FLOOR DRINKS!",
     audio:
       "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaTurnDownForWhat.mp3"
   },
   five: {
     numCards: 4,
-    response: "Jive",
+    response:
+      "Jive. Show me your dance moves. Create one dance move, and the person to right must add on top of that until someone forgets",
     audio: "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaSandstorm.mp3"
   },
   six: {
     numCards: 4,
-    response: "Chicks",
+    response: "Chicks. LADIES!!!!",
     audio:
       "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaAquaBarbieGirlBootlegCut.mp3"
   },
   seven: {
     numCards: 4,
-    response: "Heaven",
+    response: "Heaven. QUICK POINT UP. Last person to point up drinks!",
     audio: "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaRooster.mp3"
   },
   eight: {
     numCards: 4,
-    response: "Mate",
+    response:
+      "Rhyme. Okay rappers. Lets get our latest song going. Choose a word and next person rhymes with it until someone can't",
     audio:
       "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaPriceIsRightFail.mp3"
   },
   nine: {
     numCards: 4,
-    response: "Rhyme",
+    response:
+      "Rhyme. Okay rappers. Lets get our latest song going. Choose a word and next person rhymes with it until someone can't",
     audio:
       "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaYeahShittyFlute.mp3"
   },
   ten: {
     numCards: 4,
-    response: "Categories",
+    response:
+      "Categories. Choose a category and each person must name an item in that category. First person to not name an item drinks",
     audio:
       "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaBakerStreet.mp3"
   },
   jack: {
     numCards: 4,
-    response: "Men",
+    response: "Men. GET DRINKING DUDES",
     audio: "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaBruh.mp3"
   },
   queen: {
@@ -69,7 +74,8 @@ module.exports = {
   },
   king: {
     numCards: 5,
-    response: "Rule Maker",
+    response:
+      "Rule Maker. Make a rule everyone must follow for the rest of the game! Unless its the last king you drink the cup!",
     audio:
       "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaLilJonYeah.mp3"
   },
