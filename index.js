beca149a492826d457617a40c9f8b17cca2e5db1
Alex <alexanderhapp@me.com>
2017-10-14 20:04:41 -0700

    Create cards.js file

--- index.js
+++ index.js
@@ -1,82 +1,6 @@
 var alexa = require("alexa-app");
+var cards = require("./cards");
 var kingsCup = new alexa.app("kingscup");
-
-var cards = {
-  ace: {
-    numCards: 4,
-    response: "Waterfall",
-    audio:
-      "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaTheBadTouchTechno.mp3"
-  },
-  two: {
-    numCards: 4,
-    response: "You",
-    audio:
-      "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaSadTrombone.mp3"
-  },
-  three: {
-    numCards: 4,
-    response: "Me",
-    audio:
-      "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaExtendedAirhorn.mp3"
-  },
-  four: {
-    numCards: 4,
-    response: "Floor",
-    audio:
-      "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaTurnDownForWhat.mp3"
-  },
-  five: {
-    numCards: 4,
-    response: "Jive",
-    audio: "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaSandstorm.mp3"
-  },
-  six: {
-    numCards: 4,
-    response: "Chicks",
-    audio:
-      "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaAquaBarbieGirlBootlegCut.mp3"
-  },
-  seven: {
-    numCards: 4,
-    response: "Heaven",
-    audio: "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaRooster.mp3"
-  },
-  eight: {
-    numCards: 4,
-    response: "Mate",
-    audio:
-      "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaPriceIsRightFail.mp3"
-  },
-  nine: {
-    numCards: 4,
-    response: "Rhyme",
-    audio:
-      "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaYeahShittyFlute.mp3"
-  },
-  ten: {
-    numCards: 4,
-    response: "Categories",
-    audio:
-      "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaBakerStreet.mp3"
-  },
-  jack: {
-    numCards: 4,
-    response: "Men",
-    audio: "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaBruh.mp3"
-  },
-  queen: {
-    numCards: 4,
-    response: "Question Master",
-    audio: "https://s3.us-east-2.amazonaws.com/alexakingscup/yakety.mp3"
-  },
-  king: {
-    numCards: 5,
-    response: "Rule Maker",
-    audio:
-      "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaLilJonYeah.mp3"
-  }
-};
 
 kingsCup.launch(function(req, res) {
   // Store cards in a session
@@ -169,9 +93,8 @@ kingsCup.intent(
     var response = card[randCard].response;
     var audioClip = card[randCard].audio;
     if (randCard === "king" && card[randCard].numCards === 1) {
-      response = `the last King! DRINK THE CUP!`;
-      audioClip =
-        "https://s3.us-east-2.amazonaws.com/alexakingscup/AlexaSadTrombone.mp3";
+      response = card["lastKing"].response;
+      audioClip = card["lastKing"].audio;
     }
     var Speech = require("ssml-builder");
     var speech = new Speech()
